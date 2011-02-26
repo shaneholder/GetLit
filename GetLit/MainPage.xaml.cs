b4af433334e54d5cef8fa29543477de5217a9293
Michael L Perry <mperry@mallardsoft.com>
2011-02-26 10:47:02 -0600

    Added GetLit.Netflix project. Invoke OData service.

--- GetLit/MainPage.xaml.cs
+++ GetLit/MainPage.xaml.cs
@@ -9,11 +9,16 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
+using GetLit.Netflix;
+using GetLit.RIA.Server.Web;
+using System.Data.Services.Client;
 
 namespace GetLit
 {
     public partial class MainPage : UserControl
     {
+        private DataServiceCollection<Title> _titles;
+
         public MainPage()
         {
             InitializeComponent();
@@ -21,8 +26,22 @@ namespace GetLit
 
         private void UserControl_Loaded(object sender, RoutedEventArgs e)
         {
-            GetLit.RIA.Server.Web.LibraryContext context = new GetLit.RIA.Server.Web.LibraryContext();
+            LibraryContext context = new LibraryContext();
             context.Load(context.LibrariesQuery());
+
+            NetflixCatalog catalog = new Netflix.NetflixCatalog(new Uri("http://odata.netflix.com/catalog", UriKind.Absolute));
+            _titles = new DataServiceCollection<Title>(catalog);
+            string searchTerm = "goon";
+            _titles.LoadAsync(new Uri(String.Format("/Titles?$filter=Rating eq 'PG' and substringof('{0}',Name) eq true&$orderby=AverageRating desc&$top=20", searchTerm), UriKind.Relative));
+            _titles.LoadCompleted += new EventHandler<LoadCompletedEventArgs>(titles_LoadCompleted);
+        }
+
+        void titles_LoadCompleted(object sender, LoadCompletedEventArgs e)
+        {
+            if (e.Error != null)
+            {
+                System.Windows.MessageBox.Show(e.Error.ToString(), "Netflix Error", MessageBoxButton.OK);
+            }
         }
 
         private void libraryDomainDataSource_LoadedData(object sender, System.Windows.Controls.LoadedDataEventArgs e)
